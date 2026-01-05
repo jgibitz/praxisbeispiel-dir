b008398add086e2b973a7b134516f82a196f1218
jgibitz <e12428716@student.tuwien.ac.at>
2026-01-05 16:46:38 +0100

    Create headerFooter.js

---
+++ headerFooter.js
@@ -0,0 +1,12 @@
+const header = document.querySelector('#header');
+const footer = document.querySelector('#footer');
+
+window.addEventListener('load', function(){
+  this.document.head.insertAdjacentHTML('beforeend', '<link rel="stylesheet" href="style.css"/>');
+
+  header.className = 'header';
+  
+  header.innerHTML = `
+  <a href="index.html">Home</a>
+  `
+});
