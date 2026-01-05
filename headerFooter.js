e8aaf8196dc21f7ee0fc93598c20d0b8df4fb3e4
jgibitz <e12428716@student.tuwien.ac.at>
2026-01-05 19:50:14 +0100

    Update headerFooter.js

--- headerFooter.js
+++ headerFooter.js
@@ -2,9 +2,10 @@ const header = document.querySelector('#header');
 const footer = document.querySelector('#footer');
 
 window.addEventListener('load', function(){
-  this.document.head.insertAdjacentHTML('beforeend', '<link rel="stylesheet" href="style.css"/>');
+  
   
   header.innerHTML = `
   <a href="index.html">Home</a>
   `;
+  this.document.head.insertAdjacentHTML('beforeend', '<link rel="stylesheet" href="style.css"/>');
 });
